4bd23a9bb5ded86a5e3385508f0e09b786acebf6
Matthew Murray <matthewtmurray@gmail.com>
2020-09-13 12:00:53 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { Request, Response} from 'express';
+import * as routes from './routes';
+
+const app = express();
+const port = 3000;
+
+routes.register(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+
+export const register = (app: express.Application)=>{
+    app.get("/todaysActivity",(req:express.Request,res:express.Response)=>{
+        res.status(200).send({"totalActivity":"Nothing"});
+    });
+    app.get("/weeksActivity",(req:express.Request,res:express.Response)=>{
+        res.status(200).send({"totalActivity":"Nothing"});
+    });
+}
