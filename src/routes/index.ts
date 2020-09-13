6d18410f516c4dabc0b67aa18c25715672f358db
Matthew Murray <matthewtmurray@gmail.com>
2020-09-13 12:01:40 +0100

    update text

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,9 @@ import * as express from 'express';
 
 export const register = (app: express.Application)=>{
     app.get("/todaysActivity",(req:express.Request,res:express.Response)=>{
-        res.status(200).send({"totalActivity":"Nothing"});
+        res.status(200).send({"totalActivity":"fuckall"});
     });
     app.get("/weeksActivity",(req:express.Request,res:express.Response)=>{
-        res.status(200).send({"totalActivity":"Nothing"});
+        res.status(200).send({"totalActivity":"fuckall"});
     });
 }
